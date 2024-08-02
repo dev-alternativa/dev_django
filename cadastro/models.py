8a49f8c2f4a4534b2205318780bcb452e554f560
= <=>
2024-08-02 14:45:42 -0300

    Mudança do tipo de dados de alguns campos nos models de cadastro

--- cadastro/models.py
+++ cadastro/models.py
@@ -47,7 +47,7 @@ class Unidade(Base):
 
 class Categoria(Base):
     nome = models.CharField('Nome', max_length=100)
-    descricao = models.TextField('Descrição', null=True)
+    descricao = models.TextField('Descrição', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Categoria'
@@ -59,7 +59,7 @@ class Categoria(Base):
 
 class SubCategoria(Base):
     nome = models.CharField('Nome', max_length=100)
-    descricao = models.TextField('Descricao', null=True)
+    descricao = models.TextField('Descricao', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Sub-Categoria'
@@ -77,8 +77,8 @@ class ConfCoordenada(Base):
     )
 
     titulo = models.CharField('Configuração de Coordenada', max_length=100)
-    unidade = models.ForeignKey('cadastro.Unidade', verbose_name='Unidade', on_delete=models.CASCADE)
-    predio = models.CharField('Prédio', choices=PREDIO, max_length=50, null=True)
+    unidade = models.ForeignKey('cadastro.Unidade', verbose_name='Unidade', on_delete=models.PROTECT)
+    predio = models.CharField('Prédio', choices=PREDIO, max_length=50, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Configuração de Coordenada'
@@ -91,13 +91,13 @@ class ConfCoordenada(Base):
 class Transportadora(Base):
     nome = models.CharField('Nome Fantasia', max_length=100)
     cnpj = models.CharField('CNPJ / CPF', max_length=20)
-    cod_omie_COM = models.CharField('Cód. OMIE COM', max_length=15, null=True)
-    cod_omie_IND = models.CharField('Cód. OMIE IND', max_length=15, null=True)
-    cod_omie_PRE = models.CharField('Cód. OMIE PRE', max_length=15, null=True)
-    cod_omie_MRX = models.CharField('Cód. OMIE MRX', max_length=15, null=True)
-    cod_omie_SRV = models.CharField('Cód. OMIE SRV', max_length=15, null=True)
-    cod_omie_FLX = models.CharField('Cód. OMIE FLX', max_length=15, null=True)
-    obs = models.TextField('Observações', null=True, max_length=200)
+    cod_omie_COM = models.CharField('Cód. OMIE COM', max_length=15, null=True, blank=True)
+    cod_omie_IND = models.CharField('Cód. OMIE IND', max_length=15, null=True, blank=True)
+    cod_omie_PRE = models.CharField('Cód. OMIE PRE', max_length=15, null=True, blank=True)
+    cod_omie_MRX = models.CharField('Cód. OMIE MRX', max_length=15, null=True, blank=True)
+    cod_omie_SRV = models.CharField('Cód. OMIE SRV', max_length=15, null=True, blank=True)
+    cod_omie_FLX = models.CharField('Cód. OMIE FLX', max_length=15, null=True, blank=True)
+    obs = models.TextField('Observações', null=True, blank=True, max_length=200)
 
     class Meta:
         verbose_name = 'Transportadora'
@@ -130,13 +130,15 @@ class Transportadora(Base):
 
 class Prazo(Base):
     descricao = models.CharField('Descrição', max_length=120)
-    parcelas = models.CharField('Parcelas', max_length=60, null=True)
+    parcelas = models.CharField('Parcelas', max_length=60)
     codigo = models.CharField('Código', max_length=60)
 
     class Meta:
         verbose_name = 'Prazo'
         verbose_name_plural = 'Prazos'
-        unique_together = ('parcelas', 'codigo')
+        constraints = [
+            models.UniqueConstraint(fields=['parcelas', 'codigo'], name='unique_parcelas_codigo')
+        ]
 
     def __str__(self):
         return self.descricao
@@ -160,20 +162,20 @@ class ClienteFornecedor(Base):
     estado = models.CharField('Estado', choices=ESTADOS_BRASIL, max_length=50)
     endereco = models.CharField('Endereço', max_length=100)
     bairro = models.CharField('Bairro', max_length=50)
-    complemento = models.CharField('Complemento', max_length=50, null=True)
+    complemento = models.CharField('Complemento', max_length=50, null=True, blank=True)
     numero = models.CharField('Número', max_length=15)
-    telefone = models.CharField('Telefone', max_length=15, null=True)
-    ddd = models.CharField('DDD', max_length=4, null=True)
+    telefone = models.CharField('Telefone', max_length=15, null=True, blank=True)
+    ddd = models.CharField('DDD', max_length=4, null=True, blank=True)
     cep = models.CharField('CEP', max_length=12)
     email = models.CharField('E-mail', max_length=300)
-    nome_contato = models.CharField('Nome Contato', max_length=50, null=True)
+    nome_contato = models.CharField('Nome Contato', max_length=50, null=True, blank=True)
     tipo_frete = models.CharField('Tipo Frete', choices=TIPO_FRETE, max_length=100)
-    taxa_frete = models.CharField('Taxa de frete', max_length=10, null=True)
-    cliente_transportadora = models.ForeignKey('cadastro.Transportadora', verbose_name='Transportadora', on_delete=models.CASCADE)
-    prazo = models.ForeignKey('cadastro.Prazo', verbose_name='Prazo', on_delete=models.CASCADE)
+    taxa_frete = models.CharField('Taxa de frete', max_length=10, null=True, blank=True)
+    cliente_transportadora = models.ForeignKey('cadastro.Transportadora', verbose_name='Transportadora', on_delete=models.PROTECT, null=True, blank=True)
+    prazo = models.ForeignKey('cadastro.Prazo', verbose_name='Prazo', on_delete=models.PROTECT, null=True, blank=True)
     categoria = models.ManyToManyField(Categoria, related_name='clientes')
-    inscricao_estadual = models.CharField('Inscrição Estadual',max_length=20, null=True)
-    limite_credito = models.CharField('Limite de Crédito', max_length=20, null=True)
+    inscricao_estadual = models.CharField('Inscrição Estadual',max_length=20, null=True, blank=True)
+    limite_credito = models.CharField('Limite de Crédito', max_length=20, null=True, blank=True)
     # sub_categoria = models.ManyToManyField(SubCategoria, related_name='clientes')
     # tipo_produto = models.CharField('Tipo de Produto', max_length=50)
     # status = models.BooleanField('Ativo', default=True) # Verificar quis são os status
@@ -231,29 +233,29 @@ class Produto(Base):
         ('ESTOQUE', 'Estoque'),
     )
 
-    tipo_categoria = models.ForeignKey('cadastro.Categoria', verbose_name='Tipo Categoria',max_length=100, on_delete=models.CASCADE) #FK
+    tipo_categoria = models.ForeignKey('cadastro.Categoria', verbose_name='Tipo Categoria',max_length=100, on_delete=models.PROTECT) #FK
     sub_categoria = models.CharField('Sub-Categoria', max_length=100)
     nome_produto = models.CharField('Nome Produto', max_length=100)
-    largura = models.CharField('Largura', max_length=20, null=True)
-    comprimento = models.CharField('Comprimento', max_length=10, null=True)
-    m_quadrado = models.CharField('Metro Quadrado', max_length=10, null=True) # campo calculado
-    qtd_por_caixa = models.PositiveIntegerField('Quantidade por caixa', null=True)
-    peso_unitario = models.CharField('Peso Unitário', max_length=10, null=True)
-    peso_caixa = models.CharField('Peso da Caixa', max_length=10, null=True)
-    situacao = models.CharField('Estado', choices=SITUACAO_PRODUTO, max_length=50, null=True)
-    fornecedor = models.ForeignKey('cadastro.ClienteFornecedor', verbose_name='Fornecedor', max_length=100, on_delete=models.CASCADE)
-    cod_omie_com = models.CharField('Cód. OMIE CNPJ COM', max_length=30, null=True)
-    cod_oculto_omie_com = models.CharField('Cód. oculto no OMIE CNPJ COM', max_length=30, null=True)
-    cod_omie_ind = models.CharField('Cód. OMIE CNPJ IND', max_length=30, null=True)
-    cod_oculto_omie_ind = models.CharField('Cód. oculto no OMIE CNPJ IND', max_length=30, null=True)
-    cod_omie_flx = models.CharField('Cód. OMIE CNPJ FLX', max_length=30, null=True)
-    cod_oculto_omie_flx = models.CharField('Cód. oculto no OMIE CNPJ FLX', max_length=30, null=True)
-    cod_omie_pre = models.CharField('Cód. OMIE CNPJ PRE', max_length=30, null=True)
-    cod_oculto_omie_pre = models.CharField('Cód. oculto no OMIE CNPJ PRE', max_length=30, null=True)
-    cod_omie_mrx = models.CharField('Cód. OMIE CNPJ MRX', max_length=30, null=True)
-    cod_oculto_omie_mrx = models.CharField('Cód. oculto no OMIE CNPJ MRX', max_length=30, null=True)
-    cod_omie_srv = models.CharField('Cód. OMIE CNPJ SRV', max_length=30, null=True)
-    cod_oculto_omie_srv = models.CharField('Cód. oculto no OMIE CNPJ SRV', max_length=30, null=True)
+    largura = models.CharField('Largura', max_length=20, null=True, blank=True)
+    comprimento = models.CharField('Comprimento', max_length=10, null=True, blank=True)
+    m_quadrado = models.CharField('Metro Quadrado', max_length=10, null=True, blank=True) # campo calculado
+    qtd_por_caixa = models.PositiveIntegerField('Quantidade por caixa', null=True, blank=True)
+    peso_unitario = models.CharField('Peso Unitário', max_length=10, null=True, blank=True)
+    peso_caixa = models.CharField('Peso da Caixa', max_length=10, null=True, blank=True)
+    situacao = models.CharField('Estado', choices=SITUACAO_PRODUTO, max_length=50, null=True, blank=True)
+    fornecedor = models.ForeignKey('cadastro.ClienteFornecedor', verbose_name='Fornecedor', max_length=100, on_delete=models.PROTECT)
+    cod_omie_com = models.CharField('Cód. OMIE CNPJ COM', max_length=30, null=True, blank=True)
+    cod_oculto_omie_com = models.CharField('Cód. oculto no OMIE CNPJ COM', max_length=30, null=True, blank=True)
+    cod_omie_ind = models.CharField('Cód. OMIE CNPJ IND', max_length=30, null=True, blank=True)
+    cod_oculto_omie_ind = models.CharField('Cód. oculto no OMIE CNPJ IND', max_length=30, null=True, blank=True)
+    cod_omie_flx = models.CharField('Cód. OMIE CNPJ FLX', max_length=30, null=True, blank=True)
+    cod_oculto_omie_flx = models.CharField('Cód. oculto no OMIE CNPJ FLX', max_length=30, null=True, blank=True)
+    cod_omie_pre = models.CharField('Cód. OMIE CNPJ PRE', max_length=30, null=True, blank=True)
+    cod_oculto_omie_pre = models.CharField('Cód. oculto no OMIE CNPJ PRE', max_length=30, null=True, blank=True)
+    cod_omie_mrx = models.CharField('Cód. OMIE CNPJ MRX', max_length=30, null=True, blank=True)
+    cod_oculto_omie_mrx = models.CharField('Cód. oculto no OMIE CNPJ MRX', max_length=30, null=True, blank=True)
+    cod_omie_srv = models.CharField('Cód. OMIE CNPJ SRV', max_length=30, null=True, blank=True)
+    cod_oculto_omie_srv = models.CharField('Cód. oculto no OMIE CNPJ SRV', max_length=30, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Produto'
@@ -269,13 +271,13 @@ class Lote(Base):
     pedido = models.CharField('Número do Pedido', max_length=20)
     cliente = models.CharField('ID do Cliente', max_length=20)
     data_recebimento = models.DateField('Data de Recebimento')
-    tipo = models.CharField('Tipo', max_length=20, null=True)
+    tipo = models.CharField('Tipo', max_length=20, null=True, blank=True)
     container = models.CharField('Número do Container', max_length=20)
     volume = models.CharField('Volume', max_length=20)
     pallet = models.IntegerField('Número do Pallet')
     peso = models.CharField('Peso', max_length=10)
     nf = models.CharField('Nota Fiscal', max_length=50)
-    obs = models.CharField('Observações', max_length=200)
+    obs = models.CharField('Observações', max_length=200, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Lote'
