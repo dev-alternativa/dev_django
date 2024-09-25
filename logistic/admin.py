0732d1a9886a24f16c2c40594ca635f3fccc206f
= <=>
2024-09-25 08:08:03 -0300

    Adicionado Prazo ao admin/Ajustes em templates de saída

--- logistic/admin.py
+++ logistic/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from logistic.models import LeadTime
 
-# Register your models here.
+
+@admin.register(LeadTime)
+class LeadTimeAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao', 'parcelas')
+    search_fields = ('codigo', 'descricao', 'parcelas')
+    list_filter = ('codigo', 'parcelas')
